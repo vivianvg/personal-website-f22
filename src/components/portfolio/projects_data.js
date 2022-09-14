9228bebc7c6a5885afdff510b34f427bd688464e
Vivian <info.vivianguo@gmail.com>
2022-09-14 12:42:16 -0400

    profile section adjustments

--- src/components/portfolio/projects_data.js
+++ src/components/portfolio/projects_data.js
@@ -4,7 +4,7 @@ let projectElements = [
     {
         id: "project1",
         title: "Personal Website",
-        description: "A project to introduce myself to react",
+        description: "A project to introduce myself to react and animation with Framer Motion library",
         cover: "",
         date: "Aug - Sep 2022",
         tag1: "React",
@@ -15,7 +15,7 @@ let projectElements = [
         id: "project2",
         title: "UW CS Class Profile",
         description: "Associated with Graphic designer/Design lead role in UW Computer Science Club: " +
-                    "collaborated in a team of four to design light and dark UI for a class profile site in Figma" ,
+                    "collaborated in a team of four to design light and dark UI for a class profile site in Figma." ,
         cover: "",
         date: "Jan - Apr 2022",
         tag1: "UI Design",
@@ -25,7 +25,9 @@ let projectElements = [
     {
         id: "project3",
         title: "Safe Stock Portfolio Optimizer",
-        description: "CFM101 Group project: collaborated in a team of three ",
+        description: "CFM101 Group project: collaborated in a team of three to create a program that returns "
+        +" a safe portfolio given input of arbitrary stocks. The program uses Python (Pandas, Matplotlib, Numpy) to"
+        +" perform data analysis in Jupyter Notebook.",
         cover: "",
         date: "Dec 2021",
         tag1: "Python",
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { motion } from "framer-motion"
 import "./profile.css";
 
-import profileImage from '../../assets/profile/website-profile-3.png';
+import profileImage from '../../assets/profile/about-image.png';
 import linkedin from '../../assets/icons/linkedin.svg';
 import resume from '../../assets/icons/file-text.svg';
 
@@ -10,6 +10,7 @@ import resume from '../../assets/icons/file-text.svg';
 const Profile = () => {
   return (
     <div class="profile">
+      <div class="profile-container">
       <motion.div
         initial={{ opacity: 0}}
         animate={{opacity: 1 }}
@@ -21,22 +22,15 @@ const Profile = () => {
 
           I am a second year <a target="_blank" href="https://uwaterloo.ca/future-students/programs/computing-and-financial-management">Computing Financial Management</a> student at the University of Waterloo.
           <br /><br />
-          Across my academics, career, and personal life, I am motivated by a desire to learn and to create.
-          This motivation can take the form of <a target="_blank" href="https://github.com/vivianvg">lines of code</a>, <a target="_blank" href="https://www.behance.net/vivianvg">drawings</a>, and more.
+          With background and passion for finance and computer science, I hope to explore opportunities that develop my expertise in both fields.
           <br /><br /><br/>
-          <a class="button">Resume</a>&nbsp;&nbsp;<a class="button">LinkedIn</a>
+          <a class="button" href="https://www.linkedin.com/in/vivianvg/"  target="_blank">LinkedIn</a>
+
 
         </p>
       </motion.div>
-          
-      <motion.div 
-        initial={{ opacity: 0}}
-        animate={{opacity: 1 }}
-        transition={{duration: 1.5}}
-        viewport={{margin: "-100px 0px -80px 0px"}}
-        id="profile-image">
-        <img src={profileImage} id="profile-image-element"/>
-      </motion.div>
+
+      </div>
 
 
 
