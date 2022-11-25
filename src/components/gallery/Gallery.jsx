7838a32098b5d55d64e86abac7541296d1eda6d9
Vivian <info.vivianguo@gmail.com>
2022-11-25 09:55:13 -0500

    updated image gallery
    so that ppl don't think the filler colour blocks are errors

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -6,7 +6,9 @@ import galleryData from "./gallery_data.js";
 import galleryPhoto1 from "../../assets/gallery_photos/IMG_5376-2.jpg"
 import galleryPhoto2 from "../../assets/gallery_photos/DSCN0764.JPG"
 import galleryPhoto3 from "../../assets/gallery_photos/moon.jpg"
-import filler from "../../assets/gallery_photos/filler.png"
+import galleryPhoto4 from "../../assets/gallery_photos/IMG_1400.png"
+import galleryPhoto5 from "../../assets/gallery_photos/flowerfield.jpg"
+import galleryPhoto6 from "../../assets/gallery_photos/sheepherd.png"
 
 
 const Gallery = () => {
@@ -16,11 +18,11 @@ const Gallery = () => {
             return(
             <div class="gallery-photo">
                 <img src={galleryPhoto1} />
-                <img src={filler}/>
+                <img src={galleryPhoto4}/>
                 <img src={galleryPhoto2} /><br></br>
-                <img src={filler}/>
+                <img src={galleryPhoto5}/>
                 <img src={galleryPhoto3}/>
-                <img src={filler}/>
+                <img src={galleryPhoto6}/>
             </div>
             );}
             
